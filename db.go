47628e0c3bfc42a84deb759235a4ac4640c1f0f3
Evan Kysley <evan@kysley.com>
2021-08-22 00:54:31 -0400

    logging modCheck

--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -13,17 +14,27 @@ func Today() string {
 }
 
 func WriteCache() {
+	log.Print("Writing cache to database")
 	database, _ := sql.Open("sqlite3", "borp/sql.db")
 	defer database.Close()
 	for key, count := range borpas {
 		if count > 0 {
-			statement, _ := database.Prepare(fmt.Sprintf("UPDATE totals SET count = count + %d WHERE name = %s", count, key))
+			log.Printf("Writing emote: %s", key)
+			query := fmt.Sprintf("UPDATE totals SET count = count + %d WHERE name = '%s'", count, key)
+			statement, error := database.Prepare(query)
 			statement.Exec()
+
+			if error != nil {
+				log.Printf("Failed to update %s", key)
+			}
 		}
 	}
+	ResetCache()
 }
 
 func ResetCache() {
+	log.Print("Clearing cache")
+
 	for key := range borpas {
 		borpas[key] = 0
 	}
