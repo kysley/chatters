19cc84279a855c139a3ec326204786bae3654948
kysley <evan@kysley.com>
2022-01-31 00:28:46 -0500

    try it static

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,5 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
+
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +12,6 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
