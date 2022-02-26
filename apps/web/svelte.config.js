51c219330673972fef9aef41792558485422dfe6
kysley <evan@kysley.com>
2022-02-26 02:47:21 -0500

    try this

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,6 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-netlify';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-static';
 
 import preprocess from 'svelte-preprocess';
 
